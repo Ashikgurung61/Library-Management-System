fefd3fcf7db00c6daef3f216a85cd55fcdb2f67d
Ashik Gurung <142726061+Ashikgurung61@users.noreply.github.com>
2025-05-10 11:32:39 +0530

    Add files via upload

--- Library Management System Project/main.py
+++ Library Management System Project/main.py
@@ -4,8 +4,7 @@ from fastapi.templating import Jinja2Templates
 from starlette.status import HTTP_302_FOUND
 from sqlalchemy.orm import Session
 from database import SessionLocal
-from models import User
-from user_model import Users
+from models import User, StudentID
 from passlib.hash import bcrypt
 
 app = FastAPI()
@@ -18,7 +17,6 @@ def get_db():
     finally:
         db.close()
 
-
 @app.get("/", response_class=HTMLResponse)
 def login_form(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "error": None})
@@ -27,9 +25,20 @@ def login_form(request: Request):
 def admin_login(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
-@app.get("/login", response_class=HTMLResponse)
+@app.get("/sLogin", response_class=HTMLResponse)
 def user_login(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
+    return templates.TemplateResponse("sLogin.html", {"request": request})
+
+@app.post("/sLogin", response_class=HTMLResponse)
+def login(request: Request, user_id: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
+    std = db.query(StudentID).filter(StudentID.user_id == user_id, StudentID.password == password).first()
+    if std:
+        return RedirectResponse("/landing_page", status_code=HTTP_302_FOUND)
+    return templates.TemplateResponse("sLogin.html", {"request": request, "error": "Invalid username or password"})
+
+@app.get("/landing_page", response_class=HTMLResponse)
+def LandingPage(request: Request):
+    return templates.TemplateResponse("landing_page.html", {"request": request})
 
 @app.post("/login", response_class=HTMLResponse)
 def login(request: Request, username: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
@@ -41,3 +50,14 @@ def login(request: Request, username: str = Form(...), password: str = Form(...)
 @app.get("/dashboard", response_class=HTMLResponse)
 def dashboard(request: Request):
     return templates.TemplateResponse("dashboard.html", {"request": request})
+
+
+
+@app.get("/books", response_class= HTMLResponse)
+def booksDetail(request: Request):
+    return templates.TemplateResponse("books.html", {"request": request})
+
+@app.get("/users", response_class= HTMLResponse)
+def usersDetail(request: Request):
+    return templates.TemplateResponse("users.html", {"request": request})
+
--- Library Management System Project/models.py
+++ Library Management System Project/models.py
@@ -1,5 +1,22 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Enum, DateTime
 from database import Base
+import enum
+
+class RoleEnum(enum.Enum):
+    student = "student"
+    faculty = "faculty"
+    admin = "admin"
+
+class StudentID(Base):
+    __tablename__ = "users"
+    
+    user_id = Column(String(20), primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), unique=True)
+    phone = Column(String(15))
+    role = Column(Enum(RoleEnum))
+    registered_at = Column(DateTime)
+    password = Column(String(25))
 
 class User(Base):
     __tablename__ = "admin"
